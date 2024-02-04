275d2135d97015a66cd75958c1dffc1da970409e
Murasso <sou04136@gmail.com>
2024-02-04 12:12:56 +0900

    database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test-database', function () {
-    try {
-        DB::connection()->getPdo();
-        print_r("Connected successfully to: " . DB::connection()->getDatabaseName());
-    } catch (\Exception $e) {
-        die("Could not connect to the database.  Please check your configuration. Error:" . $e );
-    }
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
